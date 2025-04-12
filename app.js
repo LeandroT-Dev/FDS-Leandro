21f9638a533bcbcc2f6a26ac9db5be5eec579a8c
LeandroT-Dev <otahldobarejota@gmail.com>
2025-04-12 17:32:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let show = document.querySelector('.showmessage')
+let res = document.querySelector('.res')
+
+show.addEventListener('click', showMessage);
+
+function showMessage(){
+    res.innerHTML = 'Bem vindo ao meu trabalho de Fundamentos de Desenvolvimento de Software'
+
+}
